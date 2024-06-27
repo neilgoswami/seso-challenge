aa72829e8d2697feddf46ac744eb6eb5a318fc7e
Neel Goswami <neelgoswami.cs@gmail.com>
2024-06-26 19:15:20 -0700

    Synchronous solution
    - Initialized MinHeap with the first log entry from each log source
    - Pop the smallest log entry from MinHeap and print
    - Add new log entry from the same source into the MinHeap
    - Once MinHeap is empty, log stats for the solution
    
    NOTE: Using Heap we are not storing all log entries in the memory, only the first from each log source.

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,34 @@
 "use strict";
 
+const MinHeap = require("./MinHeap");
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const minHeap = new MinHeap();
+
+  // Initialize the MinHeap with the first log entry from all the log sources
+  logSources.forEach((logSource, index) => {
+    const log = logSource.pop();
+    if (log) {
+      minHeap.push({ log, sourceIndex: index });
+    }
+  });
+
+  // Process logs in chronological order using the MinHeap
+  while (!minHeap.isEmpty()) {
+    // Pop the smallest log entry from the MinHeap and print
+    const { log, sourceIndex } = minHeap.pop();
+    printer.print(log);
+
+    // Fetch the next log entry from the same log source and push it into MinHeap
+    const nextLog = logSources[sourceIndex].pop();
+    if (nextLog) {
+      minHeap.push({ log: nextLog, sourceIndex });
+    }
+  }
+
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
